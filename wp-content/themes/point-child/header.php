bcb0c29bd2b6e76215267784e4362b6af6fde0fe
tony <tony.jiang@yale.edu>
2015-03-20 02:41:19 +0000

    tony modify navbar

--- wp-content/themes/point-child/header.php
+++ wp-content/themes/point-child/header.php
@@ -43,28 +43,47 @@
 	<![endif]-->
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<?php if ( is_home() ) { ?>
-		<meta name="description" content="Yale's first social news outlet for the busy (and not so busy) Yalie-on-the-go. Articles published every Sundays and Thursdays." />
-	<?php } ?>
 	<?php mts_meta(); ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
 
 	<script>
 		var featured = 0;
+		var animating = false;
 		jQuery(function() {
-			rotate(featured);
-		});
-
-		function rotate(index) {
-			$('.featuredpost').each(function() {
-				var i = jQuery(this).attr('index'),
-						left = (i - index) * 1000;
-				jQuery(this).css('left', left.toString() + 'px');
+			jQuery('.tonyBox').each(function() {
+				jQuery(this).children().last().css('background-image', 'url(' + jQuery(this).children().first().attr('src') + ')');
 			});
+			position();
 
-			featured = index;
+			jQuery('.tonyCircle').click(function() {
+	      if(animating) return;
+	      animating = true;
+	      var clicked = jQuery(this).attr('index');
+	      featured = clicked;
+	      var cur = jQuery('.circleSelected').attr('index');
+	      var delta = (cur - clicked) * window.innerWidth;
+	      if(clicked == cur) {
+	        animating = false;
+	        return;
+	      }
+	      jQuery('.circleSelected').removeClass('circleSelected');
+	      jQuery(this).addClass('circleSelected');
+	      jQuery('.tonyBox').each(function() {
+	        jQuery(this).css('left', jQuery(this).position().left + delta + 'px');
+	      });
+	      setTimeout(function(){animating = false;}, 500);
+	    });
+		});
+		function position() {
+			jQuery('.tonyBox').each(function() {
+				var i = jQuery(this).attr('index');
+				jQuery(this).css('left', (i - featured) * window.innerWidth);
+			});
 		}
+		jQuery(window).resize(function() {
+  			position();
+		});
 	</script>
 </head>
 <body id ="blog" <?php body_class('main'); ?>>
@@ -88,7 +107,7 @@
 					<?php if( is_front_page() || is_home() || is_404() ) { ?>
 							<h1 id="logo" class="image-logo">
                                 <?php list($width, $height, $type, $attr) = getimagesize($mts_options['mts_logo']); ?>
-								<a href="<?php echo home_url(); ?>"><img src="<?php echo $mts_options['mts_logo']; ?>" alt="<?php bloginfo( 'name' ); ?>" <?php echo $attr; ?>></a>
+								<a href="<?php echo home_url(); ?>"><img src="<?php echo $mts_options['mts_logo']; ?>" alt="<?php bloginfo( 'name' ); ?>" ></a>
 							</h1><!-- END #logo -->
 					<?php } else { ?>
 						  <h2 id="logo" class="image-logo">
@@ -130,6 +149,7 @@
 					<a href="mailto:diane.kim@yale.edu"><img src="http://theboola.com/wp-content/uploads/2015/02/pic4.png" /></a>
 				</div>
 		</header>
+
 		<div class="secondary-navigation">
 				<nav id="navigation" >
 					<?php if ( has_nav_menu( 'primary-menu' ) ) { ?>
@@ -143,6 +163,7 @@
 				</nav>
 			</div>
 		</div>
+
 		<?php if ($mts_options['mts_header_adcode'] != '') { ?>
 			<div class="header-bottom-second">
 				<?php echo '<div id="header-widget-container">';
@@ -171,25 +192,27 @@
 						$slider_cat = implode(",", $mts_options['mts_featured_slider_cat']);
 						$my_query = new WP_Query('cat='.$slider_cat.'&posts_per_page=4&ignore_sticky_posts=1'); 
 						while ($my_query->have_posts()) : $my_query->the_post(); ?>
-						<?php if($i >= 1 and $i <= /*4*/1){ ?> 
-							<div class="featuredpost firstpost excerpt" index="<?php echo $i - 1; ?>">
-								<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="nofollow" id="first-thumbnail">
+						<?php if($i >= 1 and $i <= 3){ ?> 
+							<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="nofollow">
+								<div class="tonyBox" index="<?php echo $i - 1; ?>">
 									<?php if ( has_post_thumbnail() ) { ?> 
-										<?php the_post_thumbnail('bigthumb',array('title' => '')); ?>
-									<?php } else { ?>
-										<div class="featured-thumbnail">
-											<img src="<?php echo get_template_directory_uri(); ?>/images/bigthumb.png" class="attachment-featured wp-post-image" alt="<?php the_title(); ?>">
-										</div>
-									<?php } ?>
-									<p class="featured-excerpt">
-										<span class="featured-title"><?php the_title(); ?></span>
-										<span class="f-excerpt"><?php echo mts_excerpt(10);?></span>
-									</p>
-								</a>
-							</div><!--.post excerpt-->   
+											<?php the_post_thumbnail('bigthumb',array('title' => '')); ?>
+										<?php } else { ?>
+											<div class="featured-thumbnail">
+												<img src="<?php echo get_template_directory_uri(); ?>/images/bigthumb.png" class="attachment-featured wp-post-image" alt="<?php the_title(); ?>">
+											</div>
+										<?php } ?>
+									<div class="tonyImage"></div>
+								</div><!--.post excerpt-->   
+							</a>
 						<?php } ?>
 					<?php $i++; endwhile; wp_reset_query(); ?> 
 				</div>
+				<div class="tonyCircleContainer">
+						<div class="tonyCircle circleSelected" index="0"></div>
+						<div class="tonyCircle" index="1"></div>
+						<div class="tonyCircle" index="2"></div>
+					</div>
 			<?php } ?>
 		<?php }} ?>
 <?php } else { ?>
